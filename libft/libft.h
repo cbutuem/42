84351679372c4ceaba3a6706fd0ed1e0787909f2
Camila Butuem <camilabutuem@MacBook-Pro-de-Camila-2.local>
2021-08-26 17:22:41 -0300

    Quase la! Sem a funcao

--- libft/libft.h
+++ libft/libft.h
@@ -20,7 +20,6 @@
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *str, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t count, size_t size);
